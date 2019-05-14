fd39f63f1a1a00d7e620f27db4da5bd83fbc542f
posi7790 <polasp17@gmail.com>
2019-05-14 15:04:56 -0600

    Add DeclaringAndInitializingVariables content

---
+++ DeclaringAndIntializingVariables/src/academy/learnprogramming/DeclaringAndIntializingVariables.java
@@ -0,0 +1,45 @@
+package academy.learnprogramming;
+
+public class DeclaringAndIntializingVariables {
+
+    public static void main(String[] args) {
+        // declaring and initializing in two lines
+        int myNumber; // declaration
+//        System.out.println("myNumber= " + myNumber);
+        myNumber = 10; //initialization
+        System.out.println("myNumber= " + myNumber);
+
+        // one line
+        double myDouble = 7.50;
+        System.out.println("myDouble= " + myDouble);
+
+        float myFloat1, myFloat2, myFloat3;
+        float myFloat4; float myFloat5;
+
+        float myFloat6 = 5f, myFloat7 = 10f, myFloat8;
+        boolean b1, b2;
+
+//        double d1, double d2; // doesn't compile
+
+        int i1;
+        int i2;
+//        int i3; i4; // doesn't compile
+
+//        int int = 10; // doesn't compile
+        char Char;
+        char cHaR;
+
+        float okFloat;
+        double $ok2Double1;
+        double _alsoDouble;
+        float __okButNotNice$_123;
+
+        // illegal examples
+//        double 3Dpoint;
+//        double 3point;
+//        double my@street;
+//        float *$coffee;
+//        double public;
+
+    }
+}
