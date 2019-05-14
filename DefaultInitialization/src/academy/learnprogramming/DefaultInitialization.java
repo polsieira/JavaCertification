77441c5649e24f2cbb75676fd4ba6179444de2e0
posi7790 <polasp17@gmail.com>
2019-05-14 16:55:20 -0600

    Finish exercise with output of intial values for all data types

---
+++ DefaultInitialization/src/academy/learnprogramming/DefaultInitialization.java
@@ -0,0 +1,27 @@
+package academy.learnprogramming;
+
+public class DefaultInitialization {
+
+    static boolean myBoolean;
+    static byte myByte;
+    static short myShort;
+    static int myInt;
+    static long myLong;
+    static float myFloat;
+    static double myDouble;
+    static char myChar;
+
+    public static void main(String[] args) {
+
+//        System.out.println("localInt = " + localInt);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+
+    }
+}
