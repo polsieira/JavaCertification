d9d7328433a7fc119be65dc81cd185d8aba858c3
posi7790 <polasp17@gmail.com>
2019-05-15 13:50:32 -0600

    Finish Ordering Elements in Class lesson

--- OrderingElementsInClass/src/academy/learnprogramming/OrderingElementsInClass.java
+++ OrderingElementsInClass/src/academy/learnprogramming/OrderingElementsInClass.java
@@ -1,8 +1,21 @@
+//import java.util.Random; // declare packages first
+
 package academy.learnprogramming;
+
+//int myInt = 10; // variables cannot be outside of class
+
+//public void myMethod() {}  // methods must be inside class
+
+class myOtherClass {} // can have other class
+
+//public class AnotherClass {} // can only have one public class
 
 public class OrderingElementsInClass {
 
     public static void main(String[] args) {
 
     }
+
+    // PIC (package, import, class)
+
 }
