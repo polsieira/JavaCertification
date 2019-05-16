0f2af833bd1219389c3df5dac9c7b01105445d23
posi7790 <polasp17@gmail.com>
2019-05-16 11:39:25 -0600

    Finish Unary Operators Lesson

---
+++ UnaryOperators/src/academy/learnprogramming/UnaryOperator.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming;
+
+public class UnaryOperator {
+
+    public static void main(String[] args) {
+        int x = + 3; // + sign is redundant
+
+        double y = -x; // - unary operator
+
+        y = -y; // y * -1
+
+        boolean a  = true;
+        boolean b = !a; // would be false
+
+        b = !b; // would be true
+
+
+    }
+}
